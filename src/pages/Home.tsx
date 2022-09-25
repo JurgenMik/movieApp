46f0814aea1b31ad64c1b4c63703f1bdc562219b
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-25 18:06:28 +0300

    Add & Remove bookmarks from recommended movies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo} from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { FaSearch, FaTv, FaBookmark, FaRegBookmark } from 'react-icons/fa';
 import {MdLocalMovies} from 'react-icons/md';
 import data from '../data.json';
@@ -48,6 +48,14 @@ function Home() {
 
     let searched = useMemo(getSearchResult, [recommended, search]);
 
+    const handleBookmarks = (e : React.MouseEvent, info : movieInt) => {
+        if (!info.isBookmarked) {
+            setRecommended(recommended.filter((movies : any ) => movies.title !== info.title).concat({...info, isBookmarked : true}));
+        } else {
+            setRecommended(recommended.filter((movies : any ) => movies.title !== info.title).concat({...info, isBookmarked : false}))
+        }
+    }
+
     return(
         <div className="col-span-7 sm:ml-0 ml-6 mt-10">
             <div className="sm:w-1/2 w-full flex justify-center items-center text-white">
@@ -59,7 +67,7 @@ function Home() {
                 />
             </div>
             <div className="w-full mt-6">
-                <h1 className="sm:text-3xl text-xl text-white">Trending</h1>
+                <h1 className="sm:text-3xl text-2xl text-white">Trending</h1>
                 <div className="w-full flex flex-row space-x-10 mt-6">
                     {trending.map((info : any, index) => {
                         return(
@@ -87,7 +95,7 @@ function Home() {
                     })}
                 </div>
                 <div className="w-full mt-10">
-                    {!search ? <h1 className="text-white sm:text-3xl text-xl">Recommended for you</h1> :
+                    {!search ? <h1 className="text-white sm:text-3xl text-2xl">Recommended for you</h1> :
                         <h1 className="text-white sm:text-3xl text-xl">Found {searched.length} Results for '{search}'</h1>}
                     <div className="w-full grid sm:grid-cols-4 grid-cols-2">
                         {searched.map((info : any, index) => {
@@ -95,8 +103,8 @@ function Home() {
                                 <div className="w-full mt-10 relative" key={index}>
                                     <img className="rounded-lg sm:w-4/5 w-4/5 sm:h-40 h-24" src={info.thumbnail.regular.small} alt="recommended"/>
                                     <div className="flex flex-row text-sm text-slate-400 mt-2 space-x-1">
-                                        {info.isBookmarked ? <FaBookmark className="absolute top-5 sm:left-72 left-24 text-white text-lg" /> :
-                                            <FaRegBookmark className="absolute top-5 sm:left-72 left-24 text-white text-lg" />}
+                                        {info.isBookmarked ? <FaBookmark onClick={e => handleBookmarks(e, info)} className="absolute top-5 sm:left-72 left-24 text-white text-lg" /> :
+                                            <FaRegBookmark onClick={e => handleBookmarks(e, info)} className="absolute top-5 sm:left-72 left-24 text-white text-lg" />}
                                         <h1>
                                             {info.year}
                                         </h1>
